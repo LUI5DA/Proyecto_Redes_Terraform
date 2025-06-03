2138b1d7829113795ed75c2f6aa8c1a283417929
Luis David Salgado Gamez <gamezdavid208@gmail.com>
2025-06-02 22:01:51 -0600

    Update outputs.tf

--- WebService/terraform/outputs.tf
+++ WebService/terraform/outputs.tf
@@ -1,7 +1,27 @@
-output "vm0_ip" {
-  value = azurerm_public_ip.vm0_public_ip.ip_address
+# Public IP of the Load Balancer (for DNS & SSL setup)
+output "load_balancer_public_ip" {
+  value = azurerm_public_ip.lb_public_ip.ip_address
+  description = "The public IP address of the load balancer"
 }
 
-output "vm1_ip" {
-  value = azurerm_public_ip.vm1_public_ip.ip_address
+# VM0 name and private IP (optional)
+output "vm0_name" {
+  value = azurerm_linux_virtual_machine.nginx_vm[0].name
+  description = "The name of VM0"
+}
+
+output "vm1_name" {
+  value = azurerm_linux_virtual_machine.nginx_vm[1].name
+  description = "The name of VM1"
+}
+
+# SSH NAT Ports for each VM (used by Ansible over LB)
+output "vm0_ssh_port" {
+  value = azurerm_lb_nat_rule.ssh_nat_rule_vm0.frontend_port
+  description = "NAT port used to SSH into VM0"
+}
+
+output "vm1_ssh_port" {
+  value = azurerm_lb_nat_rule.ssh_nat_rule_vm1.frontend_port
+  description = "NAT port used to SSH into VM1"
 }
